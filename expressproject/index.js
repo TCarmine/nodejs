e92f6dd41a9ab777f6897f675701534d6367efaa
TCarmine <kappa85t@hotmail.it>
2019-11-20 14:06:20 +0100

    created jsconfig and disabled JavaScript and TypeScript Nightly so  VS code Intellisence worked

--- expressproject/index.js
+++ expressproject/index.js
@@ -1,6 +1,15 @@
+// import _ from 'lodash';
+
+
 const express = require('express'); 
+// const process = require('process');
 
 const app = express();
+ 
+process.
+
+console.log(app.toString);
+
 
 
 app.get('/',(req,res)=>{
@@ -11,7 +20,7 @@ app.get('/api/arraylits',(req,res)=>{
     res.send([1,2,3]);
   });
   
-
+process.
 
 app.listen(3000,()=>{
     console.log('Listen on port 3000...')
