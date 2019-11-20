1d9f06a6e359f43816425dace4bda74ddf04ae03
TCarmine <kappa85t@hotmail.it>
2019-11-20 16:06:06 +0100

    plying with get request in node

--- expressproject/index.js
+++ expressproject/index.js
@@ -5,23 +5,44 @@ const express = require('express');
 // const process = require('process');
 
 const app = express();
- 
-process.
 
-console.log(app.toString);
+const courses = [
+  { id: 1, name: 'Math'},
+  { id: 2, name:'History'},
+  { id: 3, name:'Physic'},
+  { id: 4, name:'Algebra'}
+];
 
-
-
+// get examples for Express
 app.get('/',(req,res)=>{
   res.send('Hey There!');
 });
 
-app.get('/api/arraylits',(req,res)=>{
+app.get('/api/courses',(req,res)=>{
     res.send([1,2,3]);
   });
-  
-process.
 
-app.listen(3000,()=>{
-    console.log('Listen on port 3000...')
-})
+app.get('/api/courses/:id',(req,res)=>{
+  const id = req.params.id;
+  const course = courses.find( c => c.id === parseInt(id));
+  if (!course)  res.status(404).send(`The course with the given ID:${id}  was not found`);
+   res.send(course);
+})  
+
+app.get('/api/posts/:year/:month',(req,res)=>{
+  res.send(req.params);
+})  
+
+app.get('/api/posts/',(req,res)=>{
+  res.send(req.query);
+})  
+
+
+app.get('/api/posts/:id',(req,res)=>{
+  res.send(req.query);
+})  
+
+// providing a way to use the available port
+const port = process.env.PORT  || 3000;
+
+app.listen(port, () => console.log(`Listen on port ${port}...`));
