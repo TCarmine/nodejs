81fdef8ff443a8d061b31d61fa939fe333f3ca11
TCarmine <kappa85t@hotmail.it>
2019-11-21 10:32:28 +0100

    implement client data avlidation with Joi

--- expressproject/index.js
+++ expressproject/index.js
@@ -1,5 +1,7 @@
-// import _ from 'lodash';
+// Validation package class
+const Joi = require('joi');
 
+// import _ from 'lodash';
 
 const express = require('express'); 
 // const process = require('process');
@@ -30,7 +32,7 @@ app.get('/api/courses/:id',(req,res)=>{
   const id = req.params.id;
   const course = courses.find( c => c.id === parseInt(id));
   if (!course)  res.status(404).send(`The course with the given ID:${id}  was not found`);
-   res.send(course);
+  res.send(course);
 })  
 
 app.get('/api/posts/:year/:month',(req,res)=>{
@@ -49,6 +51,23 @@ app.get('/api/posts/:id',(req,res)=>{
 // POST request
 
 app.post('/api/courses/', (req,res) => {
+  
+  // Basic validation, implemented with joi
+  
+    // Handling 400 Bad request
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  // object returned after validation
+
+  const result =  Joi.validate(req.body, schema);
+    
+  if(result.error){
+    //if 400 showing error details message
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
   const course = {
      id: courses.length + 1,
      name: req.body.name
@@ -63,3 +82,4 @@ app.post('/api/courses/', (req,res) => {
 const port = process.env.PORT  || 3000;
 
 app.listen(port, () => console.log(`Listen on port ${port}...`));
+
